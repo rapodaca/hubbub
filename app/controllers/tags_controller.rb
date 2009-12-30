017cb00ba613e820a12c9473229740e80135586b
Richard Apodaca <rapodaca@metamolecular.com>
2009-12-30 12:21:17 -0800

    make_resourceful tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,36 +1,55 @@
 class TagsController < ApplicationController
-  def index
-    @tags = Tag.find :all, :order => 'slug ASC'
-    
+  make_resourceful do
+    actions :index, :update, :edit, :destroy
+  end
+  
+  def show
+    @tag = Tag.find_by_slug! params[:id]
+    @articles = @tag.recent_articles params[:page]
     respond_to do |format|
       format.html
       format.atom { render :layout => false }
     end
   end
-
-  def show
-    @tag = Tag.find_by_slug! params[:id]
-    @articles = @tag.recent_articles params[:page]
+  
+  private
+  
+  def current_objects
+    Tag.find :all, :order => 'slug ASC'
   end
-
-  def update
-    @tag = Tag.find_by_slug! params[:id]
-    if @tag.update_attributes(params[:tag])
-      flash[:notice] = "Tag updated."
-      redirect_to tag_url(@tag)
-    else
-      render :action => 'edit', :status => :unprocessable_entity
-    end
+  
+  def current_object
+    @current_object ||= Tag.find_by_slug!(params[:id])
   end
-
-  def edit
-    @tag = Tag.find_by_slug! params[:id]
-  end
-
-  def destroy
-    tag = Tag.find_by_slug! params[:id]
-    tag.destroy
-    flash[:notice] = "Tag destroyed."
-    redirect_to tags_url
-  end
+  # 
+  # def index
+  #   @tags = Tag.find :all, :order => 'slug ASC'
+  #   
+  #   respond_to do |format|
+  #     format.html
+  #     format.atom { render :layout => false }
+  #   end
+  # end
+  # 
+  # 
+  # def update
+  #   @tag = Tag.find_by_slug! params[:id]
+  #   if @tag.update_attributes(params[:tag])
+  #     flash[:notice] = "Tag updated."
+  #     redirect_to tag_url(@tag)
+  #   else
+  #     render :action => 'edit', :status => :unprocessable_entity
+  #   end
+  # end
+  # 
+  # def edit
+  #   @tag = Tag.find_by_slug! params[:id]
+  # end
+  # 
+  # def destroy
+  #   tag = Tag.find_by_slug! params[:id]
+  #   tag.destroy
+  #   flash[:notice] = "Tag destroyed."
+  #   redirect_to tags_url
+  # end
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -5,9 +5,11 @@ describe TagsController do
     @tag = mock_model(Tag)
   end
   def find_tags
+    mock_tag
     Tag.stub!(:find).and_return [@tag]
   end
   def find_tag
+    mock_tag
     Tag.stub!(:find).and_return @tag
   end
   def tag_has_one_article
@@ -15,110 +17,32 @@ describe TagsController do
     @tag.stub!(:recent_articles).and_return [@article]
   end
   
-  def do_index; get :index; end
-  def do_show; get :show, :id => @tag.id; end
-  def do_update; put :update, :id => @tag.id; end
-  def do_edit; get :edit, :id => @tag.id; end
-  def do_destroy; delete :destroy, :id => @tag.id; end
-  
   describe "GET index" do
-    before(:each) do
-      mock_tag
-      find_tags
-    end
     it "assigns tags" do
-      do_index
+      find_tags
+      get :index
       assigns[:tags].should == [@tag]
-    end
-    describe "as atom" do
-      it "returns atom content" do
-        get :index, :format => 'atom'
-        response.content_type.should == 'application/atom+xml'
-      end
     end
   end
   
   describe "GET show" do
     before(:each) do
-      mock_tag
       find_tag
       tag_has_one_article
     end
     it "assigns tag" do
-      do_show
+      get :show, :id => @tag.id
       assigns[:tag].should == @tag
     end
     it "assigns articles" do
-      do_show
+      get :show, :id => @tag.id
       assigns[:articles].should == [@article]
     end
-  end
-  
-  describe "PUT update" do
-    before(:each) do
-      mock_tag
-      find_tag
-    end
-    it "updates tag attributes" do
-      @tag.should_receive(:update_attributes).and_return true
-      do_update
-    end
-    describe "when successful" do
-      before(:each) do
-        @tag.stub!(:update_attributes).and_return(true)
+    describe "as atom" do
+      it "returns atom content" do
+        get :show, :id => @tag.id, :format => 'atom'
+        response.content_type.should == 'application/atom+xml'
       end
-      it "flashes success" do
-        do_update
-        flash[:notice].should_not be_nil
-      end
-      it "redirects to tag url" do
-        do_update
-        response.should redirect_to(tag_url(@tag))
-      end
-    end
-    describe "when unsuccessful" do
-      before(:each) do
-        @tag.stub!(:update_attributes).and_return(false)
-      end
-      it "renders new" do
-        do_update
-        response.should render_template(:edit)
-      end
-    end
-  end
-  
-  describe "GET edit" do
-    before(:each) do
-      mock_tag
-      find_tag
-    end
-    
-    it "assigns tag" do
-      do_edit
-      assigns[:tag].should == @tag
-    end
-  end
-  
-  describe "DELETE destroy" do
-    before(:each) do
-      mock_tag
-      find_tag
-      @tag.stub!(:destroy)
-    end
-    
-    it "destroys tag" do
-      @tag.should_receive(:destroy)
-      do_destroy
-    end
-    
-    it "flashes success" do
-      do_destroy
-      flash[:notice].should_not be_blank
-    end
-    
-    it "redirects to tags url" do
-      do_destroy
-      response.should redirect_to(tags_url)
     end
   end
 end
