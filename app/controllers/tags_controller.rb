bbd42221a25e30af6b2fda6764d0906b7bdaf052
Richard Apodaca <rapodaca@metamolecular.com>
2009-12-30 12:21:58 -0800

    make_resourceful tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -21,35 +21,4 @@ class TagsController < ApplicationController
   def current_object
     @current_object ||= Tag.find_by_slug!(params[:id])
   end
-  # 
-  # def index
-  #   @tags = Tag.find :all, :order => 'slug ASC'
-  #   
-  #   respond_to do |format|
-  #     format.html
-  #     format.atom { render :layout => false }
-  #   end
-  # end
-  # 
-  # 
-  # def update
-  #   @tag = Tag.find_by_slug! params[:id]
-  #   if @tag.update_attributes(params[:tag])
-  #     flash[:notice] = "Tag updated."
-  #     redirect_to tag_url(@tag)
-  #   else
-  #     render :action => 'edit', :status => :unprocessable_entity
-  #   end
-  # end
-  # 
-  # def edit
-  #   @tag = Tag.find_by_slug! params[:id]
-  # end
-  # 
-  # def destroy
-  #   tag = Tag.find_by_slug! params[:id]
-  #   tag.destroy
-  #   flash[:notice] = "Tag destroyed."
-  #   redirect_to tags_url
-  # end
 end
