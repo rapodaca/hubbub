cd87bbe3d610054c19985466ebe78bbf0eec6bcb
Richard Apodaca <rapodaca@metamolecular.com>
2010-01-01 11:36:45 -0800

    customizable navigation

--- lib/hubbub.rb
+++ lib/hubbub.rb
@@ -2,6 +2,10 @@ module Hubbub
   begin
     Config = YAML.load_file("#{RAILS_ROOT}/config/hubbub.yml")
   rescue
-    Config = {  }
+    Config = {
+      :title => "Hubbub",
+      :tagline => "Pure Blogging Simplicity",
+      :articles_per_page => 10
+    }
   end
 end
